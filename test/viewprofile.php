a29825df46a983d679e3a84abb13ddb25e27a763
Matthew Steer <36450476+MattWithCurls@users.noreply.github.com>
2020-07-30 23:33:57 +0200

    Update viewprofile.php

--- test/viewprofile.php
+++ test/viewprofile.php
@@ -15,6 +15,7 @@ if ($_SESSION[user_name] && !empty($_SESSION[user_name]));
 <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 
 
 <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
@@ -55,7 +56,7 @@ if ($_SESSION[user_name] && !empty($_SESSION[user_name]));
                 $db = new PDO("mysql:host=localhost;dbname=matcha", "root", "123456");
                 
                $query = "SELECT * FROM users INNER JOIN  userdetails ON users.user_id = userdetails.user_det_id WHERE user_name='$username'";
-            //    $query = "SELECT * FROM photos INNER JOIN  users ON users.user_id = photos.photo_id WHERE user_name='$username'";
+               
                $result = $db->query($query);
                 while($row = $result->fetch (PDO::FETCH_ASSOC)){
                     
@@ -94,33 +95,54 @@ if ($_SESSION[user_name] && !empty($_SESSION[user_name]));
                 <h2><?php echo  $firstname;?> <?php echo $lastname;?> s profile</h2><br/>
                     <table>
                         <tr><td>Picture:</td><td><?php echo "<td><img src='data:image/jpg;base64,".base64_encode($image['userprofilepic'])."'/></td>";;?></td></tr>
+                      
                         <tr><td>Firstname:</td><td><?php echo $firstname;?></td></tr>
+                        
                         <tr><td>Lastname:</td><td><?php echo $lastname;?></td></tr>
+                        
                         <tr><td>Username:</td><td><?php echo $dbusername;?></td></tr>
+                        
                         <tr><td>Age:</td><td><?php echo $age;?></td></tr>
+                        
                         <tr><td>Gender:</td><td><?php echo $gender;?></td></tr>
+                        
                         <tr><td>Bio:</td><td><?php echo $bio;?></td></tr>
+                        
                         <tr><td>interests:</td><td><?php echo $interests;?></td></tr>
+                        
                         <tr><td>Country:</td><td><?php echo $country;?></td></tr>
+                        
                         <tr><td>Preferences:</td><td><?php echo $preferences;?></td></tr>
-
+                        
                         <tr><td>Last Activity:</td><td><?php echo $lastlogin;?></td></tr>
-                        <tr><td>Chat:</td><td></td></tr>
+                        
+                        <tr><td>Rate User:</td><td><div align="center" style="background: #ffffff ; padding: 50px;color:white;">
+                                <i class="fa fa-star fa-2x" data-index="0"></i>
+                                <i class="fa fa-star fa-2x" data-index="1"></i>
+                                <i class="fa fa-star fa-2x" data-index="2"></i>
+                                <i class="fa fa-star fa-2x" data-index="3"></i>
+                                <i class="fa fa-star fa-2x" data-index="4"></i>
+                                <br><br>
+                                <?php echo round($avg,2) ?>
+                            </div></td></tr>
+                            
+                            <br/>
+
                         <tr><td>Like:</td><td><div class="bs-example">
 	
-	<button type="button" class="btn btn-primary show-toast">Like</button>
-    <div class="toast" id="myToast" style="position: absolute; top: 0; right: 0;">
-        <div class="toast-header">
-            <strong class="mr-auto"><i class="fa fa-grav"></i> Notification</strong>
-            <button type="button" class="ml-2 mb-1 close" data-dismiss="toast">
-                <span aria-hidden="true">&times;</span>
-            </button>
-        </div>
-        <div class="toast-body">
-            <div>you liked the user now you can chat.</div>
-        </div></td></tr>
+                      <button type="button" class="btn btn-primary show-toast">Like</button>
+                        <div class="toast" id="myToast" style="position: absolute; top: 0; right: 0;">
+                            <div class="toast-header">
+                                <strong class="mr-auto"><i class="fa fa-grav"></i> Notification</strong>
+                                <button type="button" class="ml-2 mb-1 close" data-dismiss="toast">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="toast-body">
+                                <div>you liked the user now you can chat with <?php echo $username?>.</div>
+                            </div></td></tr>
 
-                        <div class="bs-example">
+                                            <div class="bs-example">
 	
 	
                         
@@ -131,9 +153,6 @@ if ($_SESSION[user_name] && !empty($_SESSION[user_name]));
 
         ?>
 
-
-        
-
 </body>
 </html>
 
@@ -143,7 +162,63 @@ if ($_SESSION[user_name] && !empty($_SESSION[user_name]));
             $("#myToast").toast('show');
         });
     });
-</script>
+    var ratedIndex = -1, uID = 0;
 
+        $(document).ready(function () {
+            resetStarColors();
+
+            if (localStorage.getItem('ratedIndex') != null) {
+                setStars(parseInt(localStorage.getItem('ratedIndex')));
+                uID = localStorage.getItem('uID');
+            }
+
+            $('.fa-star').on('click', function () {
+               ratedIndex = parseInt($(this).data('index'));
+               localStorage.setItem('ratedIndex', ratedIndex);
+               saveToTheDB();
+            });
+
+            $('.fa-star').mouseover(function () {
+                resetStarColors();
+                var currentIndex = parseInt($(this).data('index'));
+                setStars(currentIndex);
+            });
+
+            $('.fa-star').mouseleave(function () {
+                resetStarColors();
+
+                if (ratedIndex != -1)
+                    setStars(ratedIndex);
+            });
+        });
+
+        function saveToTheDB() {
+            $.ajax({
+               url: "rateuser.php",
+               method: "POST",
+               dataType: 'json',
+               data: {
+                   save: 1,
+                   uID: uID,
+                   ratedIndex: ratedIndex
+               }, success: function (r) {
+                    uID = r.id;
+                    localStorage.setItem('uID', uID);
+               }
+            });
+        }
+
+        function setStars(max) {
+            for (var i=0; i <= max; i++)
+                $('.fa-star:eq('+i+')').css('color', 'gold');
+        }
+
+        function resetStarColors() {
+            $('.fa-star').css('color', 'silver');
+        }
+    </script>
+
+
+</script>
 
 
