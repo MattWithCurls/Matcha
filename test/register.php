cc4ca7e8d18055c7d1bd22c6b06bb357e2fc374c
Matthew Steer <36450476+MattWithCurls@users.noreply.github.com>
2020-07-31 22:32:36 +0200

    Update register.php

--- test/register.php
+++ test/register.php
@@ -70,7 +70,7 @@
 					</div>
 
 					<div class="wrap-input100 validate-input" data-validate = "Password is required">
-						<input class="input100" type="password" name="password" placeholder="Password">
+						<input class="input100" type="password" name="password" placeholder="Password" minlength="8" required>
 						<span class="focus-input100"></span>
 						<span class="symbol-input100">
 							<i class="fa fa-lock" aria-hidden="true"></i>
@@ -78,7 +78,7 @@
 					</div>
 
 					<div class="wrap-input100 validate-input" data-validate = "Password is required">
-						<input class="input100" type="password" name="password_confirm" placeholder="Password">
+						<input class="input100" type="password" name="password_confirm" placeholder="Password" minlength="8" required>
 						<span class="focus-input100"></span>
 						<span class="symbol-input100">
 							<i class="fa fa-lock" aria-hidden="true"></i>
